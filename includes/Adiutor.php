05d2fa3c2858ee27986806dbf45e95b1151a858e
Vikipolimer <135986694+Vikipolimer@users.noreply.github.com>
2023-09-11 17:44:55 +0200

    Updated

--- includes/Adiutor.php
+++ includes/Adiutor.php
@@ -6,34 +6,36 @@
  * @ingroup Extensions
  */
 
- namespace MediaWiki\Extension\Adiutor;
+namespace MediaWiki\Extension\Adiutor;
 
- use MediaWiki\MediaWikiServices;
- use Title;
- 
- class Adiutor
- {
-     public static function onExtensionLoad()
-     {
-         $pageContent = [
-             'Adiutor-CSD.json' => '{"key": "value1"}',
-             'Adiutor-PMR.json' => '{"key": "value2"}',
-             'Adiutor-AIV.json' => '{"key": "value3"}',
-         ];
- 
-         $services = MediaWikiServices::getInstance();
- 
-         foreach ($pageContent as $pageTitle => $content) {
-             $title = Title::newFromText($pageTitle);
-             if (!$title->exists()) {
-                 $user = $services->getUserFactory()->newAnonymous();
-                 $updater = $services->getRevisionStore()->newPageUpdater($title, $user);
-                 $updater->setContent(ContentHandler::makeContent($content, $title));
-                 $summary = 'Initial content';
-                 $updater->setEditSummary($summary);
-                 $updater->saveRevision();
-                 $services->getMainWANObjectCache()->delete($title->getPrefixedDBKey());
-             }
-         }
-     }
- }
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Page\WikiPage;
+use MediaWiki\Slot\SlotRecord;
+use MediaWiki\Title\Title;
+use MediaWiki\Content\ContentHandlerFactory;
+use MediaWiki\Permissions\PermissionManager;
+use User;
+
+class Adiutor
+{
+    public static function onExtensionLoad()
+    {
+        $pageContent = [
+            'Adiutor-CSD.json' => '{"key": "value1"}',
+            'Adiutor-PMR.json' => '{"key": "value2"}',
+            'Adiutor-AIV.json' => '{"key": "value3"}',
+        ];
+        $user = User::newFromId(0); 
+        foreach ($pageContent as $pageTitle => $content) {
+            $pageUpdater = MediaWikiServices::getInstance()
+                ->getWikiPageFactory()
+                ->newFromTitle( $pageTitle )
+                ->newPageUpdater( $user );
+            $pageUpdater->setContent( SlotRecord::MAIN, $content );
+            $pageUpdater->saveRevision(
+                CommentStoreComment::newUnsavedComment( '' ),
+                EDIT_INTERNAL | EDIT_MINOR | EDIT_AUTOSUMMARY
+            );
+        }
+    }
+}
