bc70d767377090f7ea8bec39bcc0b94f118a8ab8
Vikipolimer <ydogu@wikimedia.org.tr>
2023-09-24 00:52:20 +0200

    Update UpdateLocalConfigurationHandler.php

--- includes/Rest/Handler/UpdateLocalConfigurationHandler.php
+++ includes/Rest/Handler/UpdateLocalConfigurationHandler.php
@@ -1,47 +1,59 @@
 <?php
+
 namespace MediaWiki\Extension\Adiutor\Rest\Handler;
 
-use MediaWiki\Rest\SimpleHandler;
 use MediaWiki\Rest\Validator\JsonBodyValidator;
+use MediaWiki\Rest\Validator\UnsupportedContentTypeBodyValidator;
+use MediaWiki\Rest\SimpleHandler;
 use Wikimedia\ParamValidator\ParamValidator;
+
+/**
+ * Example class to update local adiutor module configurations
+ */
 class UpdateLocalConfigurationHandler extends SimpleHandler
 {
-    public function run()
-    {
-        // Read the raw JSON data from the incoming request body
-        $rawJson = file_get_contents('php://input');
+	public function run()
+	{
+		$jsonData = $this->getValidatedBody();
 
-        $jsonBodyValidator = new JsonBodyValidator([
-            'configuration' => [
-                ParamValidator::PARAM_TYPE => 'string', 
-            ],
-        ]);
+		// Extract the 'configuration' property from the JSON data
+		$configurationData = $jsonData['configuration'];
+		$moduleName = $jsonData['module'];
+		// Define the path to the JSON file to be updated
+		$jsonFilePath = __DIR__ . "../../../../resources/ext.Adiutor/localization/$moduleName.json";
 
-        // Parse the JSON data into a PHP object
-        $jsonData = json_decode($rawJson);
+		// Convert the updated configuration data back to JSON with pretty-printing
+		$updatedJson = json_encode($configurationData, JSON_PRETTY_PRINT);
 
-        // Extract the 'configuration' property from the JSON data
-        $configurationData = $jsonData->configuration;
+		// Write the updated JSON data back to the specified file
+		$result = file_put_contents($jsonFilePath, $updatedJson);
 
-        // Define the path to the JSON file to be updated
-        $jsonFilePath = __DIR__ . "../../../../resources/ext.Adiutor/localization/$jsonData->module.json";
+		// Check if the write operation was successful
+		if ($result !== false) {
+			// If successful, return a success status
+			return ['status' => 'success', 'message' => $jsonData['configuration']];
+		} else {
+			// If not successful, return an error status
+			return ['status' => 'error'];
+		}
+	}
 
-        // Convert the updated configuration data back to JSON with pretty-printing
-        $updatedJson = json_encode($configurationData, JSON_PRETTY_PRINT);
-
-        // Write the updated JSON data back to the specified file
-        $result = file_put_contents($jsonFilePath, $updatedJson);
-
-        // Check if the write operation was successful
-        if ($result !== false)
-        {
-            // If successful, return a success status
-            return ['status' => 'success', 'message' => $jsonBodyValidator->configuration];
-        }
-        else
-        {
-            // If not successful, return an error status
-            return ['status' => 'error'];
-        }
-    }
+	public function getBodyValidator($contentType)
+	{
+		if ($contentType === 'application/json') {
+			return new JsonBodyValidator(
+				[
+					'module' => [
+						ParamValidator::PARAM_TYPE => 'string',
+						ParamValidator::PARAM_REQUIRED => true,
+					],
+					'configuration' => [
+						ParamValidator::PARAM_TYPE => 'string',
+						ParamValidator::PARAM_REQUIRED => true,
+					],
+				]
+			);
+		}
+		return new UnsupportedContentTypeBodyValidator($contentType);
+	}
 }
