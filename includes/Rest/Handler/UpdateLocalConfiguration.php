cd0cea703c63aa3d8f5d3907cc12e4259ca81e04
Vikipolimer <ydogu@wikimedia.org.tr>
2023-09-23 02:09:02 +0200

    Update UpdateLocalConfiguration.php

--- includes/Rest/Handler/UpdateLocalConfiguration.php
+++ includes/Rest/Handler/UpdateLocalConfiguration.php
@@ -6,18 +6,30 @@ use MediaWiki\Rest\SimpleHandler;
 
 class UpdateLocalConfiguration extends SimpleHandler {
     public function run() {
+        // Read the raw JSON data from the incoming request body
         $rawJson = file_get_contents('php://input');
+        
+        // Parse the JSON data into a PHP object
         $jsonData = json_decode($rawJson);
+        
+        // Extract the 'configuration' property from the JSON data
         $configurationData = $jsonData->configuration;
+        
+        // Define the path to the JSON file to be updated
         $jsonFilePath = __DIR__ . "../../../../resources/ext.Adiutor/localization/Rpp.json";
+        
+        // Convert the updated configuration data back to JSON with pretty-printing
         $updatedJson = json_encode($configurationData, JSON_PRETTY_PRINT);
+        
+        // Write the updated JSON data back to the specified file
         $result = file_put_contents($jsonFilePath, $updatedJson);
         
+        // Check if the write operation was successful
         if ($result !== false) {
-            // Successfully wrote the JSON file
+            // If successful, return a success status
             return ['status' => 'success'];
         } else {
-            // Failed to write the JSON file
+            // If not successful, return an error status
             return ['status' => 'error'];
         }
     }
