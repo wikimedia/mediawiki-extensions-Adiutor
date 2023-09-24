e3a3e65396213ab91f60477d5ecfc9b3607c12ea
Vikipolimer <ydogu@wikimedia.org.tr>
2023-09-24 21:39:09 +0200

    Update createSpeedyDeletionOptions.vue

--- resources/ext.Adiutor/components/createSpeedyDeletionOptions.vue
+++ resources/ext.Adiutor/components/createSpeedyDeletionOptions.vue
@@ -1,7 +1,10 @@
 <template>
     <cdx-message class="top-message">
         <strong>Create speedy deletion configuration</strong>
-        <cdx-button action="progressive" class="save-button" weight="primary"> Save configurations </cdx-button>
+        <cdx-button :action="saveButtonAction" :class="saveButtonClass" :weight="saveButtonWeight"
+            @click="saveConfiguration" :disabled="saveButtonDisabled">
+            {{ saveButtonLabel }}
+        </cdx-button>
     </cdx-message>
     <cdx-message type="warning" dismiss-button-label="Close">
         <h3>Parameters</h3>
@@ -91,13 +94,47 @@
         </cdx-field>
     </cdx-message>
     <cdx-field>
-        <cdx-label input-id="csdTemplateStartSingleReason">Deletion reasons:</cdx-label>
-        <cdx-text-area v-model="speedyDeletionReasons" aria-label="TextArea with rows" placeholder="Start typing..."
-            rows="8"></cdx-text-area>
-        <template #help-text>
-            This is a JSON content, please edit this content with any code editor, after than you can paste your updated
-            content here.
-        </template>
+        <table width="100%" id="adiutor-options-props">
+            <caption>
+                Speedy deletion reasons <cdx-button class="add-new-button" weight="quiet" @click="addNewNameSpace">Add
+                    New</cdx-button>
+            </caption>
+            <tr>
+                <th>Name Space</th>
+                <th>Name</th>
+                <th>Action</th>
+            </tr>
+            <tr v-for="(reasonNamespace, index) in speedyDeletionReasons" :key="index">
+                <td><cdx-text-input v-model="reasonNamespace.namespace" aria-label="Namespace value"
+                        :disabled="isNamespaceDisabled(reasonNamespace)"></cdx-text-input></td>
+                <td><cdx-text-input v-model="reasonNamespace.name" aria-label="Namespace data"></cdx-text-input></td>
+                <td><cdx-button action="destructive" @click="deleteNameSpace(reasonNamespace)" :disabled="isNamespaceDisabled(reasonNamespace)">Delete</cdx-button></td>
+            </tr>
+        </table>
+    </cdx-field>
+    <cdx-field v-for="(reasonNamespace, namespaceIndex) in speedyDeletionReasons" :key="'nestedTable-' + namespaceIndex">
+        <table width="100%" id="adiutor-options-props">
+            <caption>
+                Deletion reasons for {{ reasonNamespace.name }}
+                <cdx-button class="add-new-button" weight="quiet" @click="addNewReason(namespaceIndex)">Add New</cdx-button>
+            </caption>
+            <tr>
+                <th>Value</th>
+                <th>Data</th>
+                <th>Label</th>
+                <th>Help</th>
+                <th>Action</th>
+            </tr>
+            <tr v-for="(reason, reasonIndex) in reasonNamespace.reasons" :key="'reason-' + reasonIndex">
+                <td><cdx-text-input style="min-width: 50px;" v-model="reason.value" aria-label="Value"></cdx-text-input>
+                </td>
+                <td><cdx-text-input v-model="reason.data" aria-label="Data"></cdx-text-input></td>
+                <td><cdx-text-input v-model="reason.label" aria-label="Label"></cdx-text-input></td>
+                <td><cdx-text-input v-model="reason.help" aria-label="Help"></cdx-text-input></td>
+                <td><cdx-button action="destructive" @click="deleteReason(namespaceIndex, reasonIndex)">Delete</cdx-button>
+                </td>
+            </tr>
+        </table>
     </cdx-field>
 </template>
 <script>
@@ -115,7 +152,7 @@ module.exports = defineComponent({
         CdxMessage,
         CdxCheckbox,
         CdxField,
-        CdxRadio, CdxTextArea,CdxButton
+        CdxRadio, CdxTextArea, CdxButton
     },
     props: {
         framed: {
@@ -124,17 +161,17 @@ module.exports = defineComponent({
         }
     },
     setup() {
-        const speedyDeletionReasons = JSON.stringify(csdConfiguration.speedyDeletionReasons);
+        const speedyDeletionReasons = ref(csdConfiguration.speedyDeletionReasons);
         const csdTemplateStartSingleReason = ref(csdConfiguration.csdTemplateStartSingleReason);
         const csdTemplateStartMultipleReason = ref(csdConfiguration.csdTemplateStartMultipleReason);
         const speedyDeletionPolicyLink = ref(csdConfiguration.speedyDeletionPolicyLink);
         const speedyDeletionPolicyPageShortcut = ref(csdConfiguration.speedyDeletionPolicyPageShortcut);
-        const apiPostSummaryforTalkPage = csdConfiguration.apiPostSummaryforTalkPage;
-        const apiPostSummaryforLog = csdConfiguration.apiPostSummaryforLog;
+        const apiPostSummaryforTalkPage = ref(csdConfiguration.apiPostSummaryforTalkPage);
+        const apiPostSummaryforLog = ref(csdConfiguration.apiPostSummaryforLog);
         const csdNotificationTemplate = ref(csdConfiguration.csdNotificationTemplate);
-        const singleReasonSummary = csdConfiguration.singleReasonSummary;
-        const multipleReasonSummary = csdConfiguration.multipleReasonSummary;
-        const copyVioReasonValue = csdConfiguration.copyVioReasonValue;
+        const singleReasonSummary = ref(csdConfiguration.singleReasonSummary);
+        const multipleReasonSummary = ref(csdConfiguration.multipleReasonSummary);
+        const copyVioReasonValue = ref(csdConfiguration.copyVioReasonValue);
         const postfixReasonUsage = ref(csdConfiguration.postfixReasonUsage);
         const postfixReasonUsageRadios = [
             {
@@ -190,9 +227,135 @@ module.exports = defineComponent({
     },
     data() {
         return {
-
+            saveButtonLabel: 'Save configurations',
+            saveButtonAction: 'progressive',
+            saveButtonClass: 'save-button',
+            saveButtonWeight: 'primary',
+            saveButtonDisabled: false,
         };
-    }
+    },
+    methods: {
+
+        isNamespaceDisabled(reasonNamespace) {
+            const disabledNamespaces = ["general", "redirect", "other"];
+            return disabledNamespaces.includes(reasonNamespace.namespace);
+        },
+
+        addNewNameSpace() {
+            const newNameSpace = {
+                name: '',
+                namespace: '',
+                items: []
+            };
+            this.speedyDeletionReasons.push(newNameSpace);
+        },
+
+        deleteNameSpace(namespace) {
+            const index = this.speedyDeletionReasons.indexOf(namespace);
+            if (index !== -1) {
+                this.speedyDeletionReasons.splice(index, 1);
+            }
+        },
+
+        addNewReason(namespaceIndex) {
+            // Find the namespace based on the provided namespaceIndex
+            const reasonNamespace = this.speedyDeletionReasons[namespaceIndex];
+
+            if (reasonNamespace) {
+                const newReason = {
+                    namespace: reasonNamespace.namespace,
+                    value: '',
+                    data: '',
+                    selected: false,
+                    label: '',
+                    help: '',
+                };
+                reasonNamespace.reasons.push(newReason);
+            }
+        },
+
+        deleteReason(namespaceIndex, reasonIndex) {
+            // Find the namespace based on the provided namespaceIndex
+            const reasonNamespace = this.speedyDeletionReasons[namespaceIndex];
+
+            // Check if the reasonNamespace and the reason exist (a safety check)
+            if (reasonNamespace && reasonNamespace.reasons[reasonIndex]) {
+                // Use splice to remove the reason at the specified reasonIndex
+                reasonNamespace.reasons.splice(reasonIndex, 1);
+            }
+        },
+
+        async saveConfiguration() {
+            // Change the button label and disable it during the save operation
+            this.saveButtonLabel = 'Saving...';
+            this.saveButtonAction = 'default';
+            //this.saveButtonDisabled = true;
+
+            // Prepare the data to be sent in the HTTP request
+            const data = {
+                title: 'MediaWiki:AdiutorCreateSpeedyDeletion.json',
+                content: {
+                    "speedyDeletionReasons": this.speedyDeletionReasons,
+                    "csdTemplateStartSingleReason": this.csdTemplateStartSingleReason,
+                    "csdTemplateStartMultipleReason": this.csdTemplateStartMultipleReason,
+                    "postfixReasonUsage": this.postfixReasonUsage,
+                    "multipleReasonSeparation": this.multipleReasonSeparation,
+                    "speedyDeletionPolicyLink": this.speedyDeletionPolicyLink,
+                    "speedyDeletionPolicyPageShortcut": this.speedyDeletionPolicyPageShortcut,
+                    "apiPostSummaryforTalkPage": this.apiPostSummaryforTalkPage,
+                    "apiPostSummaryforLog": this.apiPostSummaryforLog,
+                    "csdNotificationTemplate": this.csdNotificationTemplate,
+                    "singleReasonSummary": this.singleReasonSummary,
+                    "multipleReasonSummary": this.multipleReasonSummary,
+                    "copyVioReasonValue": this.copyVioReasonValue,
+                }
+            };
+
+            // Define the API endpoint URL
+            const apiUrl = '/mediawiki/rest.php/adiutor/v0/updatelocalconfiguration';
+
+            try {
+                // Send a POST request to the API with the data
+                const response = await fetch(apiUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                });
+
+                if (!response.ok) {
+                    // Handle HTTP error if the response status is not OK
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                // Parse the response data as JSON
+                const responseData = await response.json();
+
+                if (responseData.status === 'success') {
+                    // Display a success notification if the update was successful
+                    mw.notify(mw.message('adiutor-localization-settings-has-been-updated').text(), {
+                        title: mw.msg('adiutor-warning'),
+                        type: 'success'
+                    });
+                    // Revert the button label and enable it after a delay (2 seconds)
+                    this.saveButtonLabel = 'Save configurations';
+                    this.saveButtonAction = 'progressive';
+                    this.saveButtonDisabled = false;
+                } else {
+                    // Log an error message if the update was not successful
+                    console.error('Error updating configuration');
+                    this.saveButtonLabel = 'Try again';
+                    this.saveButtonAction = 'destructive';
+                    this.saveButtonDisabled = false;
+                }
+            } catch (error) {
+                // Handle any fetch-related errors and log them
+                console.error('Fetch error:', error);
+            }
+        }
+
+    },
 });
 </script>
 <style lang="less">
