a9916ae0b85069dba4aa1eba423e97741c27391a
zoranzoki21 <zorandori4444@gmail.com>
2023-12-25 02:10:05 +0100

    Follow-up patch: Fix failing tests again
    Change-Id: I57286eb5224c3d7d9cfb52ef6a22be1519df67f0

--- includes/Rest/Handler/UpdateLocalConfigurationHandler.php
+++ includes/Rest/Handler/UpdateLocalConfigurationHandler.php
@@ -3,22 +3,18 @@
 namespace MediaWiki\Extension\Adiutor\Rest\Handler;
 
 use MediaWiki\CommentStore\CommentStoreComment;
-use MediaWiki\Content\Content;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Rest\SimpleHandler;
 use MediaWiki\Rest\Validator\JsonBodyValidator;
 use MediaWiki\Rest\Validator\UnsupportedContentTypeBodyValidator;
-use MediaWiki\Rest\SimpleHandler;
 use MediaWiki\Revision\SlotRecord;
-use MediaWiki\Storage\PageUpdater;
-use MediaWiki\MediaWikiServices;
-use Wikimedia\ParamValidator\ParamValidator;
 use TextContent;
-use User;
+use Wikimedia\ParamValidator\ParamValidator;
 
 /**
  * Example class to update local adiutor module configurations
  */
-class UpdateLocalConfigurationHandler extends SimpleHandler
-{
+class UpdateLocalConfigurationHandler extends SimpleHandler {
 	/**
 	 * Updates the local configuration file with the provided JSON data.
 	 *
@@ -26,30 +22,29 @@ class UpdateLocalConfigurationHandler extends SimpleHandler
 	 * - If the update is successful, the status will be 'success' and the message will be the updated content.
 	 * - If the update fails, the status will be 'error'.
 	 */
-	public function run()
-	{
+	public function run() {
 		$jsonData = $this->getValidatedBody();
 		$pageTitle = $jsonData['title'];
-		$pageContent = json_encode($jsonData['content'], JSON_PRETTY_PRINT);
+		$pageContent = json_encode( $jsonData['content'], JSON_PRETTY_PRINT );
 		$user = $this->getAuthority()->getUser();
 		$pageUpdater = MediaWikiServices::getInstance();
 		$titleFactory = $pageUpdater->getTitleFactory();
 		$pageUpdater = MediaWikiServices::getInstance()
 			->getWikiPageFactory()
-			->newFromTitle($titleFactory->newFromText($pageTitle))
-			->newPageUpdater($user);
-		$pageUpdater->setContent(SlotRecord::MAIN, new TextContent($pageContent));
+			->newFromTitle( $titleFactory->newFromText( $pageTitle ) )
+			->newPageUpdater( $user );
+		$pageUpdater->setContent( SlotRecord::MAIN, new TextContent( $pageContent ) );
 		$pageUpdater->saveRevision(
-			CommentStoreComment::newUnsavedComment('Configuration file updated'),
+			CommentStoreComment::newUnsavedComment( 'Configuration file updated' ),
 			EDIT_INTERNAL | EDIT_MINOR | EDIT_AUTOSUMMARY
 		);
 		$saveStatus = $pageUpdater->getStatus();
-		if ($saveStatus !== false) {
+		if ( $saveStatus !== false ) {
 			// If successful, return a success status
-			return ['status' => 'success', 'message' => $jsonData['content']];
+			return [ 'status' => 'success', 'message' => $jsonData['content'] ];
 		} else {
 			// If not successful, return an error status
-			return ['status' => 'error'];
+			return [ 'status' => 'error' ];
 		}
 	}
 
@@ -59,9 +54,8 @@ class UpdateLocalConfigurationHandler extends SimpleHandler
 	 * @param string $contentType The content type of the request body.
 	 * @return BodyValidatorInterface The body validator instance.
 	 */
-	public function getBodyValidator($contentType)
-	{
-		if ($contentType === 'application/json') {
+	public function getBodyValidator( $contentType ) {
+		if ( $contentType === 'application/json' ) {
 			return new JsonBodyValidator(
 				[
 					'title' => [
@@ -75,6 +69,6 @@ class UpdateLocalConfigurationHandler extends SimpleHandler
 				]
 			);
 		}
-		return new UnsupportedContentTypeBodyValidator($contentType);
+		return new UnsupportedContentTypeBodyValidator( $contentType );
 	}
 }
