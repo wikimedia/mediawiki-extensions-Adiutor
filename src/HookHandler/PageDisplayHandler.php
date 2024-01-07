959de35c83f4a008924016dcc76e708ae692d93b
Dogu <ydogu@wikimedia.org.tr>
2024-01-07 04:10:53 +0100

    Refactor PageDisplayHandler.php for Adiutor extension
    This patch refactors the PageDisplayHandler.php file in the
    src/HookHandler directory of the Adiutor extension. The changes
    include the addition of the OutputPage class use statement,
    improvements in code structure, and the extraction of the logic
    for loading modules and HTML content into a separate method named
    loadModulesAndHTML. This method is now called within the
    onBeforePageDisplay handler to enhance readability and maintainability.
    
    Additionally, a new private method getConfigData is introduced to
    handle the retrieval of configuration data from the cache or
    generation if not available. This method is used to load configuration
    data for various Adiutor features from specific MediaWiki pages.
    
    The patch aims to improve code organization and readability, making it
    easier to understand and maintain the PageDisplayHandler.php file.
    
    Change-Id: I67b09c8aaceabd96fe59daa1cd014c616125e727

--- src/HookHandler/PageDisplayHandler.php
+++ src/HookHandler/PageDisplayHandler.php
@@ -7,6 +7,7 @@ use FormatJson;
 use MediaWiki\Extension\Adiutor\Utils\Utils;
 use MediaWiki\Hook\BeforePageDisplayHook;
 use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\Output\OutputPage;
 use MediaWiki\Permissions\PermissionManager;
 use MediaWiki\Revision\RevisionLookup;
 use MediaWiki\Revision\RevisionRecord;
@@ -70,96 +71,108 @@ class PageDisplayHandler implements BeforePageDisplayHook {
 	public function onBeforePageDisplay( $out, $skin ): void {
 		$extensionRegistry = ExtensionRegistry::getInstance();
 		$user = $out->getUser();
+		$title = $out->getTitle();
 
-		if ( !Utils::isEnabledForUser( $this->userOptionsLookup,
-			$user,
-			$extensionRegistry ) ) {
+		if ( $title->isSpecialPage() ) {
+			$this->loadModulesAndHTML( $out );
 			return;
 		}
 
-		if ( !$this->userOptionsLookup->getOption( $user,
-			'adiutor-enable' ) ) {
+		if ( !Utils::isEnabledForUser( $this->userOptionsLookup, $user, $extensionRegistry ) ) {
 			return;
 		}
-		if ( !$this->permissionManager->userHasRight( $user,
-			'edit' ) ) {
+		if ( !$this->userOptionsLookup->getOption( $user, 'adiutor-enable' ) ) {
 			return;
 		}
+		if ( !$this->permissionManager->userHasRight( $user, 'edit' ) ) {
+			return;
+		}
+
+		$this->loadModulesAndHTML( $out );
+	}
+
+	/**
+	 * Loads modules and HTML content for the page display.
+	 *
+	 * @param OutputPage $out The OutputPage object.
+	 *
+	 * @return void
+	 */
+	private function loadModulesAndHTML( OutputPage $out ): void {
 		$out->addModules( 'ext.adiutor' );
-		$out->addHTML( $this->templateParser->processTemplate( 'Adiutor',
-			[] ) );
-		$configData =
-			$this->wanObjectCache->getWithSetCallback( $this->wanObjectCache->makeKey( 'Adiutor',
-				'config-data' ),
-				$this->wanObjectCache::TTL_HOUR,
-				function () {
-					$configData = [];
-					$configPages = [
-						[
-							'title' => 'MediaWiki:AdiutorRequestPageProtection.json',
-							'configuration' => 'adiutorRequestPageProtection',
-						],
-						[
-							'title' => 'MediaWiki:AdiutorCreateSpeedyDeletion.json',
-							'configuration' => 'adiutorCreateSpeedyDeletion',
-						],
-						[
-							'title' => 'MediaWiki:AdiutorDeletionPropose.json',
-							'configuration' => 'adiutorDeletionPropose',
-						],
-						[
-							'title' => 'MediaWiki:AdiutorRequestPageMove.json',
-							'configuration' => 'adiutorRequestPageMove',
-						],
-						[
-							'title' => 'MediaWiki:AdiutorArticleTagging.json',
-							'configuration' => 'adiutorArticleTagging',
-						],
-					];
-					foreach ( $configPages as $configPage ) {
-						if ( !isset( $configPage['title'] ) || !isset( $configPage['configuration'] ) ) {
-							$this->logger->warning( 'Configuration page data is incomplete', [ 'configPage' => $configPage ] );
-							continue;
-						}
-						$title = Title::newFromText( $configPage['title'] );
-						if ( !$title ) {
-							$this->logger->warning( 'Configuration page title is invalid',
-								[ 'configPageTitle' => $configPage['title'] ] );
-							continue;
-						}
-						$rev = $this->revisionLookup->getRevisionByTitle( $title );
-						if ( !$rev ) {
-							$this->logger->warning( 'Configuration page not found',
-								[ 'configPageTitle' => $configPage['title'] ] );
-							$configData[$configPage['configuration']] = [];
-							continue;
-						}
-						$content =
-							$rev->getContent( SlotRecord::MAIN,
-								RevisionRecord::RAW );
-						if ( !( $content instanceof TextContent ) ) {
-							$this->logger->warning( 'Configuration page content is not TextContent',
-								[
-									'configPageTitle' => $configPage['title'],
-									'contentType' => get_class( $content ),
-								] );
-							$configData[$configPage['configuration']] = [];
-							continue;
-						}
-						$jsonContent =
-							FormatJson::decode( $content->getText(),
-								true );
-						if ( !is_array( $jsonContent ) ) {
-							$this->logger->warning( 'Configuration page content is not valid JSON',
-								[ 'configPageTitle' => $configPage['title'] ] );
-							$configData[$configPage['configuration']] = [];
-							continue;
-						}
-						$configData[$configPage['configuration']] = $jsonContent;
-					}
-
-					return $configData;
-				} );
+		$out->addHTML( $this->templateParser->processTemplate( 'Adiutor', [] ) );
+		$configData = $this->getConfigData();
 		$out->addJsConfigVars( $configData );
+	}
+
+	/**
+	 * Retrieves the configuration data from the cache or generates it if not available.
+	 *
+	 * @return array The configuration data.
+	 */
+	private function getConfigData(): array {
+		return $this->wanObjectCache->getWithSetCallback(
+			$this->wanObjectCache->makeKey( 'Adiutor', 'config-data' ),
+			$this->wanObjectCache::TTL_HOUR,
+			function () {
+				$configData = [];
+				$configPages = [
+					[
+						'title' => 'MediaWiki:AdiutorRequestPageProtection.json',
+						'configuration' => 'adiutorRequestPageProtection',
+					],
+					[
+						'title' => 'MediaWiki:AdiutorCreateSpeedyDeletion.json',
+						'configuration' => 'adiutorCreateSpeedyDeletion',
+					],
+					[
+						'title' => 'MediaWiki:AdiutorDeletionPropose.json',
+						'configuration' => 'adiutorDeletionPropose',
+					],
+					[
+						'title' => 'MediaWiki:AdiutorRequestPageMove.json',
+						'configuration' => 'adiutorRequestPageMove',
+					],
+					[
+						'title' => 'MediaWiki:AdiutorArticleTagging.json',
+						'configuration' => 'adiutorArticleTagging',
+					],
+				];
+				foreach ( $configPages as $configPage ) {
+					if ( !isset( $configPage['title'] ) || !isset( $configPage['configuration'] ) ) {
+						$this->logger->warning( 'Configuration page data is incomplete', [ 'configPage' => $configPage ] );
+						continue;
+					}
+					$title = Title::newFromText( $configPage['title'] );
+					if ( !$title ) {
+						$this->logger->warning( 'Configuration page title is invalid', [ 'configPageTitle' => $configPage['title'] ] );
+						continue;
+					}
+					$rev = $this->revisionLookup->getRevisionByTitle( $title );
+					if ( !$rev ) {
+						$this->logger->warning( 'Configuration page not found', [ 'configPageTitle' => $configPage['title'] ] );
+						$configData[$configPage['configuration']] = [];
+						continue;
+					}
+					$content = $rev->getContent( SlotRecord::MAIN, RevisionRecord::RAW );
+					if ( !( $content instanceof TextContent ) ) {
+						$this->logger->warning( 'Configuration page content is not TextContent', [
+							'configPageTitle' => $configPage['title'],
+							'contentType' => get_class( $content ),
+						] );
+						$configData[$configPage['configuration']] = [];
+						continue;
+					}
+					$jsonContent = FormatJson::decode( $content->getText(), true );
+					if ( !is_array( $jsonContent ) ) {
+						$this->logger->warning( 'Configuration page content is not valid JSON', [ 'configPageTitle' => $configPage['title'] ] );
+						$configData[$configPage['configuration']] = [];
+						continue;
+					}
+					$configData[$configPage['configuration']] = $jsonContent;
+				}
+				return $configData;
+			}
+		);
 	}
 }
