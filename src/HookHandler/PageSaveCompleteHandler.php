d906ccddcc37ce4339c952f27449b3c7d2ffccde
Dogu <ydogu@wikimedia.org.tr>
2024-01-16 16:53:43 +0100

    Implement PageSaveCompleteHook in PageSaveCompleteHandler
    This change updates the PageSaveCompleteHandler within the extension to implement the PageSaveCompleteHook from MediaWiki\Storage\Hook.
    
    The onPageSaveComplete method signature has been updated to match the interface, and method documentation has been altered to a simple
    inheritDoc tag, indicating that the information comes from the
    interface itself.
    
    Cache invalidation for 'Adiutor', 'config-data' occurs within the
    method after a page save completes.
    
    Change-Id: I808866a94b458a747542410c522a8a88ad46bd30

--- src/HookHandler/PageSaveCompleteHandler.php
+++ src/HookHandler/PageSaveCompleteHandler.php
@@ -3,30 +3,14 @@
 namespace MediaWiki\Extension\Adiutor\HookHandler;
 
 use MediaWiki\MediaWikiServices;
-use MediaWiki\Revision\RevisionRecord;
-use MediaWiki\Storage\EditResult;
-use User;
-use WikiPage;
+use MediaWiki\Storage\Hook\PageSaveCompleteHook;
 
-class PageSaveCompleteHandler {
+class PageSaveCompleteHandler implements PageSaveCompleteHook {
 
 	/**
-	 * This method is called after a page is saved in MediaWiki.
-	 * It deletes a specific cache key from the main WAN object cache.
-	 *
-	 * @param WikiPage $wikiPage The WikiPage object representing the saved page.
-	 * @param User $user The User object representing the user who saved the page.
-	 * @param string $summary The summary of the page edit.
-	 * @param int $flags Flags indicating additional information about the save operation.
-	 * @param RevisionRecord $revisionRecord The RevisionRecord object representing the saved revision.
-	 * @param EditResult $editResult The EditResult object representing the result of the save operation.
-	 *
-	 * @return void
+	 * @inheritDoc
 	 */
-	public static function onPageSaveComplete(
-		WikiPage $wikiPage, User $user, string $summary, int $flags,
-		RevisionRecord $revisionRecord, EditResult $editResult
-	) {
+	public function onPageSaveComplete( $wikiPage, $user, $summary, $flags, $revisionRecord, $editResult ) {
 		$wanObjectCache = MediaWikiServices::getInstance()->getMainWANObjectCache();
 		$key = $wanObjectCache->makeKey( 'Adiutor', 'config-data' );
 		$wanObjectCache->delete( $key );
