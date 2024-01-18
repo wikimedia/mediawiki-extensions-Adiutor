63cc4b34b7cde86d8665777900b85558e2b73ed2
Doğu Abaris <ydogu@wikimedia.org.tr>
2024-01-18 19:38:11 +0100

    Remove invalid argument from getModulePolicy function call
    Change-Id: I45db47ad0228f24124d56ed36a46b57f09443ca1

--- resources/ext.adiutor/components/createSpeedyDeletion.vue
+++ resources/ext.adiutor/components/createSpeedyDeletion.vue
@@ -89,7 +89,7 @@
         </template>
       </cdx-checkbox>
       <!-- eslint-disable vue/no-v-html -->
-      <span v-html="getModulePolicy( $i18n( 'please-read-the-x-policy' ) )"></span>
+      <span v-html="getModulePolicy"></span>
     </template>
   </cdx-dialog>
 </template>
